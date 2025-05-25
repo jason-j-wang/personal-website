5f0d690874953280685879ef60688f13bcc1714f
jason-j-wang <jason.wang014@gmail.com>
2025-05-25 15:36:57 -0700

    added routing, new components for each page

--- personal-website/src/app/app.routes.ts
+++ personal-website/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { ProjectComponent } from './projects/project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectComponent}
+];
--- personal-website/src/app/navbar/nav-bar.component.ts
+++ personal-website/src/app/navbar/nav-bar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
