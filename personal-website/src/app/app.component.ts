283c05e03a286b70e7e57aad9390972f9319b942
jason-j-wang <jason.wang014@gmail.com>
2025-06-07 17:27:47 -0700

    apply prettier formatting

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavBarComponent } from './navbar/nav-bar.component';
   selector: 'app-root',
   imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'personal-website';
