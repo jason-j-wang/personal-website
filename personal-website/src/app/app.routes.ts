e561ccc74933a6aeecd38d809383f301ebc7fc52
jason-j-wang <jason.wang014@gmail.com>
2025-06-25 21:30:50 -0700

    added experience tab

--- personal-website/src/app/app.routes.ts
+++ personal-website/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home/home.component";
 import { ProjectComponent } from "./projects/project/project.component";
+import { ExperienceComponent } from "./experience/experience/experience.component";
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
+    { path: "experience", component: ExperienceComponent },
     { path: "projects", component: ProjectComponent },
 ];
