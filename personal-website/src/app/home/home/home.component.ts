6410195469e46e34100b5a36d88a1574f1f536d8
jason-j-wang <jason.wang014@gmail.com>
2025-08-06 21:04:46 -0700

    added text for home page

--- personal-website/src/app/home/home/home.component.ts
+++ personal-website/src/app/home/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
     selector: "home",
-    imports: [],
+    imports: [FontAwesomeModule],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent {
+    faGithub = faGithub;
+    faLinkedin = faLinkedin;
+}
